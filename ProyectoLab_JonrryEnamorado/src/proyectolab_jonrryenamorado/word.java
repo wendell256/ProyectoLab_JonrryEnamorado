b5c1e98a3449f5b1f76999eae08ee3bcfd9517ac
Jonrry <jonrry.enamorado@unitec.edu>
2018-06-29 20:14:09 -0600

    Creacion con avance

---
+++ ProyectoLab_JonrryEnamorado/src/proyectolab_jonrryenamorado/word.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectolab_jonrryenamorado;
+
+/**
+ *
+ * @author wende
+ */
+public class word {
+    private String word;
+    private int count;
+
+    public word() {
+    }
+
+    public word(String word, int count) {
+        this.word = word;
+        this.count = count;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    @Override
+    public String toString() {
+        return word + " = " + count;
+    }
+    
+    
+}
